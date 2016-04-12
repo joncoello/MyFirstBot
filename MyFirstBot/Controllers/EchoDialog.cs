efba22d7cf2af5204e47e6cb4a0a6860849df5ea
Jon Coello <joncoello@hotmail.com>
2016-04-12 20:52:07 +0100

    add stateful logic

--- MyFirstBot/Controllers/EchoDialog.cs
+++ MyFirstBot/Controllers/EchoDialog.cs
@@ -9,6 +9,8 @@ namespace MyFirstBot
     [Serializable]
     public class EchoDialog : IDialog<object>
     {
+        private int count;
+
         public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -16,7 +18,29 @@ namespace MyFirstBot
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
         {
             var message = await argument;
-            await context.PostAsync("You said: " + message.Text);
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(context, AfterResetAsync, "reset well ?", "well not know !");
+            }
+            else
+            {
+                await context.PostAsync(string.Format("{0}: well {1}", this.count++, message.Text));
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        private async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("well reset");
+            }
+            else
+            {
+                await context.PostAsync("not reset well");
+            }
             context.Wait(MessageReceivedAsync);
         }
     }
